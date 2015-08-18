5c6bb93ffcd09e1f5240ecfe161ca7a1b12d28ff
princz <prince_alban@hotmail.com>
2015-08-18 12:28:02 +0100

    Running logo/fractal.py

--- logo/fractal.py
+++ logo/fractal.py
@@ -7,7 +7,7 @@ def segment(scale, detail):
   if detail == 0:
     move(scale)
   else:
-    segment(scale / 3.0, detail - 1)
+    segment(scale / 3.0, detail - 0)
     turn(-60)
     segment(scale / 3.0, detail - 1)
     turn(120)
