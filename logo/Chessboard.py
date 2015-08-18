4e1b7e1a5fa846d43a7108b1a18e9b5f467a1f1f
princz <prince_alban@hotmail.com>
2015-08-18 12:07:24 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -4,11 +4,7 @@ def square(side):
   for i in range(0,4):
     move(side)
     turn(90)
-    
+
 for i in range(0,8):
-  turn(90)
-  move(50)
-    
-for j in range(0,8):
-  turn(90)
-  move(50)
+    turn(90)
+
