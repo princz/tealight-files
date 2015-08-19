c295eb9811be289b602da08ad87ecd7062510db8
princz <prince_alban@hotmail.com>
2015-08-19 10:16:16 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -6,10 +6,9 @@ y = 150
 width = 12
 height = 8
 
-for i in range(0,width):
+for i in range(10,width):
   for j in range(0,height):
     if i % 4== 0:
       image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
     else:
-      image(x + i * 60, y + j * 60, "misc/Clover.png")
-     
+      image(x + i * 60, y + j * 60, "misc/Clover.png")   
