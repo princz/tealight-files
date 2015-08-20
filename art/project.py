bc5f15adcc5a31bd50a154e3d5c492e0eed44806
princz <prince_alban@hotmail.com>
2015-08-20 15:01:12 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -11,3 +11,33 @@ while (Nc == 0):
 
 p=float(Nc)/float(r*c)
 
+def isMine(x,y):
+  if x < 0 or x > 16:
+    return 0
+  if y < 0 or y > 16:
+    return 0
+  return m[x][y]
+
+def check(x,y):
+
+  count = 0 
+
+  if isMine(x-1, y-1) == 1:
+    count=count+1
+  if isMine(x, y-1) == 1:  
+    count=count+1
+  if isMine(x+1, y-1) == 1:    
+    count=count+1
+  if isMine(x+1, y) == 1:    
+    count=count+1
+  if isMine(x+1, y+1) == 1:    
+    count=count+1  
+  if isMine(x, y+1) == 1:    
+    count=count+1
+  if isMine(x-1, y+1) == 1:    
+    count=count+1
+  if isMine(x-1, y) == 1:    
+    count=count+1
+    
+  return count
+
