c266abb171f364a045157e23624d4f362b56713d
princz <prince_alban@hotmail.com>
2015-08-20 12:03:58 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -7,5 +7,10 @@ Nc=0
 while (Nc == 0):
   Nc=int(random()*HighestNoMines)
 
-for i in range(0,Nc):
-  print i
+#for i in range(0,Nc):
+
+for j in range(0,r-1):
+  for k in range(0,c-1):
+    m[r][c]=0
+    
+print m
