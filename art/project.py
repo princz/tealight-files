1e65c0363b2725c2d90eb9dc210499b317f86cf0
princz <prince_alban@hotmail.com>
2015-08-20 10:30:58 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -2,7 +2,7 @@ from tealight.art import (line, spot, circle, box, rectangle, image, text, backg
 
 grid = []
 for row in range(64):
-  grid.append(0)
+  grid(row).append([])
      
 for column in range(10):
   grid(row).append([])
