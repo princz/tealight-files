8b79fc92c18a914b0d6f0c034c42274de89e8a56
princz <prince_alban@hotmail.com>
2015-08-20 10:52:57 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,7 +1,5 @@
-table= [ [ 0 for i in range(6) ] for j in range(6) ]
-print table
-for d1 in range(6):
-    for d2 in range(6):
-        table[d1][d2]= d1+d2+2
-print table
- 
+rows = 10
+cols= 10
+
+a=[]
+for row in xrange(rows: a += [[0]*cols]
