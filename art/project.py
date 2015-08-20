3ec0bf3b0721a2d7de99a2c7b285f074bac8d640
princz <prince_alban@hotmail.com>
2015-08-20 10:50:29 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,7 +1,7 @@
 table= [ [ 0 for i in range(6) ] for j in range(6) ]
 print table
-for d1 in range(10):
-    for d2 in range(10):
+for d1 in range(6):
+    for d2 in range(6):
         table[d1][d2]= d1+d2+2
 print table
  
