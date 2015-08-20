bda9cd5ab17124f2d88ac6d00cb36bddeb4357fa
princz <prince_alban@hotmail.com>
2015-08-20 15:00:51 +0100

    Running art/project.py

--- art/project.py
+++ art/project.py
@@ -1,2 +1,13 @@
 from random import random
 #from github.jackm110.art.Project import *
+
+r=16
+c=16
+HighestNoMines = 30
+
+Nc=0
+while (Nc == 0):
+  Nc=int(random()*HighestNoMines)
+
+p=float(Nc)/float(r*c)
+
